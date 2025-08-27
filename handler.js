e821202fab0d3175e9ee0ae49e0a62d0a47be7ba
Gor Babayan <babayangor868@gmail.com>
2025-08-27 18:06:20 +0400

    serverless latest version

--- handler.js
+++ handler.js
@@ -1,6 +1,6 @@
-'use strict';
+export const hello = async (event) => {
+  console.log("Event: ", event);
 
-module.exports.hello = async () => {
   return {
     statusCode: 200,
     body: JSON.stringify({ message: "Hello world" }),
