197c5e3bf1cb1c914f638edf6e18da9c48fe701b
Gor Babayan <babayangor868@gmail.com>
2025-09-03 10:50:01 +0400

    stored props in DynamoDB

--- myLambdaFunc/handler.js
+++ myLambdaFunc/handler.js
@@ -1,3 +1,5 @@
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { GetCommand, PutCommand, DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
 import {
     CognitoIdentityProviderClient,
     SignUpCommand,
@@ -5,10 +7,13 @@ import {
     ConfirmSignUpCommand
 } from "@aws-sdk/client-cognito-identity-provider";
 
+
 const client = new CognitoIdentityProviderClient({ region: "us-east-1" });
+const dynamodbClient = new DynamoDBClient({ region: "us-east-1" });
+const docClient = DynamoDBDocumentClient.from(dynamodbClient);
 
 export const signUp = async (event) => {
-    const { email, password } = JSON.parse(event.body);
+    const { email, password, firstName, lastName, role, language, nationality } = JSON.parse(event.body);
 
     try {
         const command = new SignUpCommand({
@@ -20,11 +25,26 @@ export const signUp = async (event) => {
 
         const response = await client.send(command);
 
+        const putCommand = new PutCommand({
+            TableName: process.env.USERS_TABLE,
+            Item: {
+                email,
+                firstName,
+                lastName,
+                role: "user",
+                language: language || "en",
+                nationality: nationality || "unknown",
+            }
+        });
+
+        await docClient.send(putCommand);
+
         return {
             statusCode: 200, 
             body: JSON.stringify({ message: "User registered", response }),
         }
     } catch(err) {
+        console.error("Sign up error: ", err);
         return {
             statusCode: 400,
             body: JSON.stringify({ error: err.message }),
@@ -68,11 +88,23 @@ export const login = async (event) => {
 
         const response = await client.send(command);
 
+        const getUserCommand = new GetCommand({
+            TableName: process.env.USERS_TABLE,
+            Key: { email: email },
+        });
+
+        const userResult = await docClient.send(getUserCommand);
+
         return {
             statusCode: 200,
-            body: JSON.stringify({ message: "Login succesful", tokens: response.AuthenticationResult }),
+            body: JSON.stringify({ 
+                message: "Login successful", 
+                tokens: response.AuthenticationResult,
+                user: userResult.Item || {},
+            }),
         };
     } catch(err) {
+        console.error("Login error: ", err);
         return {
             statusCode: 400,
             body: JSON.stringify({ error: err.message }),
