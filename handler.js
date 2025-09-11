ff9a3b6c3f8be5917e61d7cc49baa49616613d34
Gor Babayan <babayangor868@gmail.com>
2025-09-11 17:27:11 +0400

    s3 uploaded object sqs trigger

--- handler.js
+++ handler.js
@@ -1,8 +1,32 @@
-export const hello = async (event) => {
-  console.log("Event: ", event);
+import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
+import { getSignedUrl as generateSignedUrl } from "@aws-sdk/s3-request-presigner";
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: "Hello world" }),
-  };
-};
+const s3 = new S3Client({ region: process.env.AWS_REGION });
+export const getSignedUrl = async (event) => {
+    try {
+        const fileName = event.queryStringParameters?.fileName;
+        const contentType = event.queryStringParameters?.contentType;
+
+        const key = `uploads/${fileName}`;
+
+        const command = new PutObjectCommand({
+            Bucket: process.env.BUCKET_NAME,
+            Key: fileName,
+            ContentType: contentType,
+        });
+
+        const signedUrl = await generateSignedUrl(s3, command, { expiresIn: 60 });
+
+        return {
+            statusCode: 200,
+            headers: { "Access-Control-Allow-Origin": "*" },
+            body: JSON.stringify({ uploadUrl: signedUrl }),
+        };
+    } catch(error) {
+        console.error(error);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: "Not signed url" }),
+        };
+    }
+} 
